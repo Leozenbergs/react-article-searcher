daff5e704c5d2b1d0046a9f2f5186ededc3b1a4e
leozenbergs <leoabreu14@gmail.com>
2019-07-29 11:41:51 -0300

    initial commit

---
+++ build/precache-manifest.ecd145128c55dc7944aca8e6c368a119.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038d7883075ae5a14edc58ce3ca04232",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05e84e8e60de1b7f5458",
+    "url": "/static/css/main.7788385c.chunk.css"
+  },
+  {
+    "revision": "05e6776b91d038177904",
+    "url": "/static/js/2.01420b0e.chunk.js"
+  },
+  {
+    "revision": "05e84e8e60de1b7f5458",
+    "url": "/static/js/main.8d314f56.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
